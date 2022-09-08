f4a17db58c7980cfc1735d602573376ec551bac5
felipemarinhodev <felipmarinho@gmail.com>
2022-09-08 09:40:59 -0700

    a1.7 - include types that will be returned and function of seeing more

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,22 +1,39 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import IRestaurante from '../../interfaces/IRestaurante';
+import { IPaginacao } from '../../interfaces/IPaginacao';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
 
 const ListaRestaurantes = () => {
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
+  const [proximaPagina, setProximaPagina] = useState<string>('');
 
   useEffect(() => {
     axios
-      .get('http://localhost:8000/api/v1/restaurantes/')
+      .get<IPaginacao<IRestaurante>>(
+        'http://localhost:8000/api/v1/restaurantes/'
+      )
       .then((response) => {
+        setProximaPagina(response.data.next);
         setRestaurantes(response.data.results);
       })
       .catch((error) => {
         console.error(error);
       });
   }, []);
+
+  const verMais = () => {
+    axios
+      .get<IPaginacao<IRestaurante>>(proximaPagina)
+      .then((response) => {
+        setProximaPagina(response.data.next);
+        setRestaurantes([...restaurantes, ...response.data.results]);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   return (
     <section className={style.ListaRestaurantes}>
@@ -26,6 +43,7 @@ const ListaRestaurantes = () => {
       {restaurantes?.map((item) => (
         <Restaurante restaurante={item} key={item.id} />
       ))}
+      {proximaPagina && <button onClick={verMais}>Ver Mais</button>}
     </section>
   );
 };
