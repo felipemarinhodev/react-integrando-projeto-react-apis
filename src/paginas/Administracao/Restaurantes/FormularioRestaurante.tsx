b35feed69f68756f176626192ce3ecb542de4546
felipemarinhodev <felipmarinho@gmail.com>
2022-09-09 09:53:05 -0700

    a3.2 - update value

--- src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,19 +1,47 @@
 import { Button, TextField } from '@mui/material';
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import IRestaurante from '../../../interfaces/IRestaurante';
 
 const FormularioRestaurante = () => {
+  const params = useParams();
   const [nomeRestaurante, setNomeRestaurante] = useState('');
+
+  useEffect(() => {
+    if (params.id) {
+      axios
+        .get<IRestaurante>(
+          `http://localhost:8000/api/v2/restaurantes/${params.id}/`
+        )
+        .then((result) => {
+          setNomeRestaurante(result.data.nome);
+        });
+    }
+  }, [params]);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    axios
-      .post('http://localhost:8000/api/v2/restaurantes/', {
-        nome: nomeRestaurante,
-      })
-      .then(() => {
-        alert(`Restaurante: ${nomeRestaurante} foi cadastrado com sucesso!`);
-      });
+    if (params.id) {
+      axios
+        .put<IRestaurante>(
+          `http://localhost:8000/api/v2/restaurantes/${params.id}/`,
+          {
+            nome: nomeRestaurante,
+          }
+        )
+        .then(() => {
+          alert(`Restaurante: ${nomeRestaurante} foi atualizado com sucesso!`);
+        });
+    } else {
+      axios
+        .post('http://localhost:8000/api/v2/restaurantes/', {
+          nome: nomeRestaurante,
+        })
+        .then(() => {
+          alert(`Restaurante: ${nomeRestaurante} foi cadastrado com sucesso!`);
+        });
+    }
   };
 
   return (
@@ -22,6 +50,7 @@ const FormularioRestaurante = () => {
         id="standard-basic"
         label="Nome"
         variant="standard"
+        value={nomeRestaurante}
         onChange={(event) => setNomeRestaurante(event.target.value)}
       />
       <Button type="submit" variant="outlined">
