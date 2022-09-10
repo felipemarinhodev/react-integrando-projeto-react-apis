59d64b5133c83b5fc57a33fcfd247610649a897e
felipemarinhodev <felipmarinho@gmail.com>
2022-09-10 09:27:42 -0700

    a4.2 - improve layout

--- src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,4 +1,4 @@
-import { Button, TextField } from '@mui/material';
+import { Box, Button, TextField, Typography } from '@mui/material';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
@@ -45,18 +45,31 @@ const FormularioRestaurante = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <TextField
-        id="standard-basic"
-        label="Nome"
-        variant="standard"
-        value={nomeRestaurante}
-        onChange={(event) => setNomeRestaurante(event.target.value)}
-      />
-      <Button type="submit" variant="outlined">
-        Salvar
-      </Button>
-    </form>
+    <Box
+      sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
+      <Typography component="h1" variant="h6">
+        Formulário de Restaurantes
+      </Typography>
+      <Box component="form" onSubmit={handleSubmit}>
+        <TextField
+          label="Nome do Restaurante"
+          variant="standard"
+          value={nomeRestaurante}
+          onChange={(event) => setNomeRestaurante(event.target.value)}
+          fullWidth
+          required
+        />
+        <Button
+          type="submit"
+          variant="outlined"
+          fullWidth
+          sx={{ marginTop: 1 }}
+        >
+          Salvar
+        </Button>
+      </Box>
+    </Box>
   );
 };
 
